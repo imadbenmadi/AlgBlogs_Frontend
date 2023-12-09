e6835d63c3ff17a1fce4c0858bce274341745d53
imadbenmadi <imadbenmadi891@gmail.com>
2023-12-09 14:07:56 +0100

    init home navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-
+import Home from './Components/Pages/Home'
 function App() {
   
 }
---
+++ src/Components/Pages/Home.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import NavBar from '../Shared Components/NavBar'
+export default function Home() {
+  return (
+    <div>Home</div>
+  )
+}
