6857e3aa8d28ddacca9f55e840f9009756d48b1a
imadbenmadi <imadbenmadi891@gmail.com>
2023-12-09 14:50:26 +0100

    fixing nav bar Authenthication

--- src/Components/Shared/NavBar.jsx
+++ src/Components/Shared/NavBar.jsx
@@ -1,60 +1,67 @@
-import React from 'react'
-import { useState } from 'react';
+import React, { useEffect, useState } from "react";
 import { CiSearch } from "react-icons/ci";
 import { IoNotifications } from "react-icons/io5";
-import { useEffect } from 'react';
+
 export default function NavBar() {
-    
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [data, setdata] = useState(false);
-    function Nav(isAuthenticated) {
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const [data, setdata] = useState(null); // Change the initial state to null
+
+    function Nav() {
         return (
-            <div className=' bg-red-600'>
-                <img src="http://localhost:3000/Logo.png" alt="Logo" />
+            <div className="bg-gray-800 text-white p-10 flex items-center">
+                <img
+                    src="http://localhost:3000/Logo.png"
+                    alt="Logo"
+                    className="w-10 h-10" // Adjusted width and added height for a square logo
+                />
                 {/* search bar */}
-                <div>
-                    <CiSearch />
-                    <input type="text" />
+                <div className="ml-4">
+                    <CiSearch className="text-white" />
+                    <input type="text" className="ml-2 p-2" />
                 </div>
-                <div>
+                <div className="ml-auto">
                     {isAuthenticated ? (
-                        <div>
-                            <div>Write a Blog</div>
-                            <img src={data.UserName} alt="Profile pic"></img>
-                            <IoNotifications />
+                        <div className="flex items-center">
+                            <div className="mr-4">Write a Blog</div>
+                            <img
+                                src={data?.userName} // Update to use correct property name
+                                alt="Profile pic"
+                                className="rounded-full max-w-20 h-20" // Adjusted width and added height for a square profile pic
+                            />
+                            <IoNotifications className="ml-4" />
                         </div>
                     ) : (
                         <div>Login</div>
                     )}
                 </div>
-            </div>);
-  }
-  useEffect(() => {
-       const checkAuthStatus = async () => {
-           try {
-               const response = await fetch("http://localhost:3000/authStatus"); // Adjust the URL based on your server configuration
+            </div>
+        );
+    }
 
-               // Check if the response is successful (status code 200)
-               if (response.ok) {
-                   const data = await response.json();
-                   setdata(data)
-                   setIsAuthenticated(data.isAuthenticated);
-               } else {
-                   // Handle non-successful response (e.g., status code 404, 500)
-                   setIsAuthenticated(false);
-               }
-           } catch (error) {
-               console.error("Error checking authentication status:", error);
-               setIsAuthenticated(false);
-           }
-       };
+    useEffect(() => {
+        const checkAuthStatus = async () => {
+            try {
+                const response = await fetch(
+                    "http://localhost:3000/authStatus"
+                );
 
-       checkAuthStatus();
-  }, []);
-    return (
-        <>
-            sd
-            {<Nav isAuthenticated={isAuthenticated} />};
-        </>
-    );
+                if (response.ok) {
+                    const userData = await response.json();
+                    setIsAuthenticated(userData.isAuthenticated);
+                    setdata(userData);
+                } else {
+                    setIsAuthenticated(false);
+                    setdata(null);
+                }
+            } catch (error) {
+                console.error("Error checking authentication status:", error);
+                setIsAuthenticated(false);
+                setdata(null);
+            }
+        };
+
+        checkAuthStatus();
+    }, []);
+
+    return <>{<Nav />}</>; // Removed unnecessary curly braces
 }
